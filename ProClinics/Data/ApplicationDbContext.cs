4bc042e3c8236bc000ef7b1b759265c4207171ab
Renato Ramon <renatophoenix123@gmail.com>
2024-09-05 13:09:53 -0300

    feat: Add ApplicationDbContext with DbSets and configurations  Implemented ApplicationDbContext for managing database context. Added DbSets for Speciality, Doctor, Patient, and Scheduling. Configured model creating to apply entity configurations from the assembly.

--- ProClinics/Data/ApplicationDbContext.cs
+++ ProClinics/Data/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProClinics.Models;
+using System.Reflection;
 
 namespace ProClinics.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Speciality> Specialitys { get; set; }  
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }    
+        public DbSet<Scheduling> Schedulings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
     }
 }
